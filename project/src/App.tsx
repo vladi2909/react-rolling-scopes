1b3195e1399ba2ead5776fa599a75fb8cef86921
Vladimir Kozak <uladzimirkozak@gmail.com>
2022-06-13 19:18:10 +0300

    feat: add router & material ui

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+
+import { HomePage } from './pages/Homepage';
+import { AboutUs } from './pages/AboutUspage';
+import { NotFound } from './pages/NotFoundpage';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar variant="dense">
+              <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                <Link to="/">Home</Link>
+              </IconButton>
+              <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                <Link to="/about">About Us</Link>
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+        </Box>
       </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
